e570c2999bcb4579d5fef9754369f6480e3ac0a7
Sparsh Aggarwal <aggarwalsparsh175@gmail.com>
2020-07-11 22:46:16 +0530

    First Page

--- webscrapping/laptop.py
+++ webscrapping/laptop.py
@@ -9,28 +9,46 @@ sheet = workbook['Sheet1']
 name = []
 price =[]
 rating = []
-browser = webdriver.Chrome('D:\\Work\\Work2\\chromedriver.exe')
-browser.get('https://www.amazon.in/s?k=laptop&ref=nb_sb_noss')
-page = browser.page_source
-soup = BeautifulSoup(page,features='lxml')
+_name = 1
+_price = 2
+_rating = 3
+def driver():
+    browser = webdriver.Chrome('D:\\Work\\Work2\\chromedriver.exe')
+    browser.get('https://www.amazon.in/s?k=laptop&ref=nb_sb_noss')
+    page = browser.page_source
+    soup = BeautifulSoup(page,features='lxml')
+    return soup
 def get_data(soup):
-    name = []
-    price = []
-    rating = []
+    time.sleep(5)
+    data = {}
+    data['name'] = []
+    data['price'] = []
+    data['ratings'] = []
     for i in soup.findAll(name='div', attrs={'class': 's-include-content-margin s-border-bottom s-latency-cf-section' }):
         a = i.find(name='span', attrs={
                    'class': 'a-size-medium a-color-base a-text-normal'})
         p = i.find(name='span', attrs={'class': 'a-offscreen'})
         r = i.find(name='span', attrs={'class': 'a-icon-alt'})
-        name.append(a.text)
+        data['name'].append(a.text)
         if p != None:
-            price.append(p.text)
+            data['price'].append(p.text)
+        else:
+            data['price'].append('None')
         if r != None:
-            rating.append(r.text.split(' ')[0])
-    return(name,price,rating)
-time.sleep(5)
-# name,price,rating = get_data(soup)
-def load_data(sheet,workbook):
-    row = sheet.max_row()
-    print(row)
-load_data(sheet,workbook)
+            rat = r.text.split(' ')[0]
+            data['ratings'].append(rat)
+        else:
+            data['ratings'].append('None')
+    print('Data Successfully Scrapped.')
+    return(data)
+def load_data(sheet,workbook,data):
+    row = sheet.max_row+1
+    for i in range(len(data['name'])):
+        sheet.cell(row+i,_name).value = data['name'][i]
+        sheet.cell(row+i,_price).value = data['price'][i]
+        sheet.cell(row+i,_rating).value = data['ratings'][i]
+    workbook.save(file)
+    print('File saved Successfully!')
+soup = driver()
+data = get_data(soup)
+load_data(sheet,workbook,data)
